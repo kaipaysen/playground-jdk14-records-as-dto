2f65dcac78041ea6f74192debdf0b9e46b5e88ae
Kai Paysen <kai@sekp.de>
2020-09-16 14:58:17 +0200

    fixes

--- src/main/java/de/sekp/playground/java14/records/controller/HelloController.java
+++ src/main/java/de/sekp/playground/java14/records/controller/HelloController.java
@@ -2,8 +2,10 @@ package de.sekp.playground.java14.records.controller;
 
 import javax.validation.Valid;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    
+        
     public record HelloRequest(
-        @JsonProperty("name") String name
+        String name, 
+        String zzz  // 2nd field is required by jackson
     ) {}
 
     public record HelloResponse(
@@ -21,8 +24,8 @@ public class HelloController {
     ) {}
 
     @RequestMapping(method = RequestMethod.POST)
-    public HelloResponse hello(@Valid HelloRequest query) {
+    public HelloResponse hello(@RequestBody @Valid HelloRequest query) {
         return new HelloResponse("Hello " + query.name());
     }
-    
+   
 }
